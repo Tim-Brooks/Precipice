2a7ebed0227c0fad413a9bcb6ea3cba984ff5f18
Tim Brooks <tim.brooks@staples.com>
2015-07-05 14:48:34 -0500

    Add other properties

--- src/main/java/net/uncontended/precipice/ServiceProperties.java
+++ src/main/java/net/uncontended/precipice/ServiceProperties.java
@@ -20,6 +20,7 @@ package net.uncontended.precipice;
 import net.uncontended.precipice.circuit.BreakerConfigBuilder;
 import net.uncontended.precipice.circuit.CircuitBreaker;
 import net.uncontended.precipice.circuit.DefaultCircuitBreaker;
+import net.uncontended.precipice.concurrent.PrecipiceSemaphore;
 import net.uncontended.precipice.metrics.ActionMetrics;
 import net.uncontended.precipice.metrics.DefaultActionMetrics;
 import net.uncontended.precipice.timeout.TimeoutService;
@@ -29,6 +30,8 @@ public class ServiceProperties {
     private ActionMetrics metrics = new DefaultActionMetrics();
     private CircuitBreaker breaker = new DefaultCircuitBreaker(new BreakerConfigBuilder().build());
     private TimeoutService timeoutService = TimeoutService.defaultTimeoutService;
+    private PrecipiceSemaphore semaphore = null;
+    private int concurrencyLevel = Service.MAX_CONCURRENCY_LEVEL;
 
     public void actionMetrics(ActionMetrics metrics) {
         this.metrics = metrics;
@@ -40,5 +43,13 @@ public class ServiceProperties {
 
     public void timeoutService(TimeoutService timeoutService) {
         this.timeoutService = timeoutService;
+    }
+
+    public void concurrencyLevel(int concurrencyLevel) {
+        this.concurrencyLevel = concurrencyLevel;
+    }
+
+    public void semaphore(PrecipiceSemaphore semaphore) {
+        this.semaphore = semaphore;
     }
 }
