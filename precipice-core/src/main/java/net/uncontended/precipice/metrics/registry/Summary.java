6bb96802c44fffba48b3fcdf96ed114d60f58656
Tim Brooks <tim@uncontended.net>
2016-01-11 19:14:27 -0800

    Update summary for new statuses

--- precipice-core/src/main/java/net/uncontended/precipice/metrics/registry/Summary.java
+++ precipice-core/src/main/java/net/uncontended/precipice/metrics/registry/Summary.java
@@ -31,22 +31,9 @@ public class Summary<T extends Enum<T> & SuperStatusInterface> {
     public long pendingCount = 0;
     public long remainingCapacity = 0;
 
+    public final long[] totalMetricCounts;
     public final long[] metricCounts;
-
-    public long totalSuccesses = 0;
-    public long totalErrors = 0;
-    public long totalTimeouts = 0;
-    public long totalMaxConcurrency = 0;
-    public long totalQueueFull = 0;
-    public long totalCircuitOpen = 0;
-    public long totalAllRejected = 0;
-    public long successes = 0;
-    public long errors = 0;
-    public long timeouts = 0;
-    public long maxConcurrency = 0;
-    public long queueFull = 0;
-    public long circuitOpen = 0;
-    public long allRejected = 0;
+    
     public LatencySnapshot successLatency = LatencyMetrics.DEFAULT_SNAPSHOT;
     public LatencySnapshot errorLatency = LatencyMetrics.DEFAULT_SNAPSHOT;
     public LatencySnapshot timeoutLatency = LatencyMetrics.DEFAULT_SNAPSHOT;
@@ -54,45 +41,35 @@ public class Summary<T extends Enum<T> & SuperStatusInterface> {
     public LatencySnapshot totalSuccessLatency = LatencyMetrics.DEFAULT_SNAPSHOT;
     public LatencySnapshot totalErrorLatency = LatencyMetrics.DEFAULT_SNAPSHOT;
     public LatencySnapshot totalTimeoutLatency = LatencyMetrics.DEFAULT_SNAPSHOT;
+    private final Class<T> metricType;
 
     Summary(long period, TimeUnit unit, ActionMetrics<T> metrics, Service service) {
         this.period = period;
         this.unit = unit;
-        actionMetrics = metrics;
         this.service = service;
+        actionMetrics = metrics;
         remainingCapacity = service.remainingCapacity();
-        metricCounts = new long[metrics.getMetricType().getEnumConstants().length];
+        metricType = metrics.getMetricType();
+        totalMetricCounts = new long[metricType.getEnumConstants().length];
+        metricCounts = new long[metricType.getEnumConstants().length];
     }
 
     public void refresh() {
         pendingCount = service.pendingCount();
         remainingCapacity = service.remainingCapacity();
 
-        MetricCounter<T> metricCounter = actionMetrics.totalCountMetricCounter();
+        MetricCounter<T> totalMetricCounter = actionMetrics.totalCountMetricCounter();
 
-//        totalSuccesses = metricCounter.getMetricCount(Metric.SUCCESS);
-//        totalErrors = metricCounter.getMetricCount(Metric.ERROR);
-//        totalTimeouts = metricCounter.getMetricCount(Metric.TIMEOUT);
-//        totalMaxConcurrency = metricCounter.getMetricCount(Metric.MAX_CONCURRENCY_LEVEL_EXCEEDED);
-//        totalQueueFull = metricCounter.getMetricCount(Metric.QUEUE_FULL);
-//        totalCircuitOpen = metricCounter.getMetricCount(Metric.CIRCUIT_OPEN);
-//        totalAllRejected = metricCounter.getMetricCount(Metric.ALL_SERVICES_REJECTED);
-        successes = 0;
-        errors = 0;
-        timeouts = 0;
-        maxConcurrency = 0;
-        queueFull = 0;
-        circuitOpen = 0;
-        allRejected = 0;
+        for (T t : metricType.getEnumConstants()) {
+            int metricIndex = t.ordinal();
+            metricCounts[metricIndex] = 0;
+            totalMetricCounts[metricIndex] = totalMetricCounter.getMetricCount(t);
+        }
 
         for (MetricCounter<T> m : actionMetrics.metricCounterIterable(period, unit)) {
-//            successes += m.getMetricCount(Metric.SUCCESS);
-//            errors += m.getMetricCount(Metric.ERROR);
-//            timeouts += m.getMetricCount(Metric.TIMEOUT);
-//            maxConcurrency += m.getMetricCount(Metric.MAX_CONCURRENCY_LEVEL_EXCEEDED);
-//            queueFull += m.getMetricCount(Metric.QUEUE_FULL);
-//            circuitOpen += m.getMetricCount(Metric.CIRCUIT_OPEN);
-//            allRejected += m.getMetricCount(Metric.ALL_SERVICES_REJECTED);
+            for (T t : metricType.getEnumConstants()) {
+                metricCounts[t.ordinal()] += m.getMetricCount(t);
+            }
         }
 
         LatencyMetrics latencyMetrics = service.getLatencyMetrics();
