5899fa81d0ac948bd45197e03d03efdaafbe04af
Tim Brooks <tim@uncontended.net>
2016-02-09 18:38:56 -0800

    Add comment about work to be investigated

--- precipice-core/src/main/java/net/uncontended/precipice/backpressure/BPCountMetrics.java
+++ precipice-core/src/main/java/net/uncontended/precipice/backpressure/BPCountMetrics.java
@@ -67,6 +67,8 @@ public class BPCountMetrics<T extends Enum<T>> implements BPTotalCountMetrics<T>
 
     @Override
     public void incrementMetricCount(T metric, long nanoTime) {
+        // TODO: What happens if you go backwards in time?
+
         totalCounter.incrementMetric(metric);
         MetricCounter<T> currentMetricCounter = buffer.getSlot(nanoTime);
         if (currentMetricCounter == null) {
