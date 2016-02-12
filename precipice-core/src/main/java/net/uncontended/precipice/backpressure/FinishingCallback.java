164638b596903a02bc104f62d4f4010c8ad5807d
Tim Brooks <tim@uncontended.net>
2016-02-11 20:52:46 -0800

    Implement classes to build promises or completables

---
+++ precipice-core/src/main/java/net/uncontended/precipice/backpressure/FinishingCallback.java
@@ -0,0 +1,19 @@
+package net.uncontended.precipice.backpressure;
+
+import net.uncontended.precipice.Failable;
+import net.uncontended.precipice.PerformingContext;
+import net.uncontended.precipice.PrecipiceFunction;
+
+class FinishingCallback<Result extends Enum<Result> & Failable> implements PrecipiceFunction<Result, PerformingContext> {
+    private final NewGuardRail<Result, ?> guardRail;
+
+    FinishingCallback(NewGuardRail<Result, ?> guardRail) {
+        this.guardRail = guardRail;
+    }
+
+    @Override
+    public void apply(Result result, PerformingContext context) {
+        long endTime = guardRail.getClock().nanoTime();
+        guardRail.releasePermits(context.permitCount(), result, context.startNanos(), endTime);
+    }
+}
---
+++ precipice-core/src/main/java/net/uncontended/precipice/backpressure/PromiseFactory.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2016 Timothy Brooks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package net.uncontended.precipice.backpressure;
+
+import net.uncontended.precipice.Failable;
+import net.uncontended.precipice.concurrent.Completable;
+import net.uncontended.precipice.concurrent.Eventual;
+import net.uncontended.precipice.concurrent.PrecipicePromise;
+
+public class PromiseFactory<Result extends Enum<Result> & Failable, Rejected extends Enum<Rejected>> {
+
+    private final NewGuardRail<Result, Rejected> guardRail;
+    private final FinishingCallback<Result> finishingCallback;
+
+    public PromiseFactory(NewGuardRail<Result, Rejected> guardRail) {
+        this.guardRail = guardRail;
+        finishingCallback = new FinishingCallback<>(guardRail);
+    }
+
+    public <R> Eventual<Result, R> acquirePermitsAndGetPromise(long number) {
+        return acquirePermitsAndGetPromise(number, null);
+    }
+
+    public <R> Eventual<Result, R> acquirePermitsAndGetPromise(long number, PrecipicePromise<Result, R> externalPromise) {
+        long startTime = guardRail.getClock().nanoTime();
+        Rejected rejected = guardRail.acquirePermits(number, startTime);
+        if (rejected != null) {
+            throw new BPRejectedException(rejected);
+        }
+
+        return getPromise(number, startTime, externalPromise);
+    }
+
+    public <R> Eventual<Result, R> getPromise(long number, long nanoTime) {
+        return getPromise(number, nanoTime, null);
+    }
+
+    public <R> Eventual<Result, R> getPromise(long number, long nanoTime, Completable<Result, R> externalCompletable) {
+        Eventual<Result, R> promise = new Eventual<>(number, nanoTime, externalCompletable);
+        promise.internalOnComplete(finishingCallback);
+        return promise;
+    }
+}
