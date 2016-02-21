1a696157b1f5c459328b57f28b35554fd5f68c39
Tim Brooks <tim@uncontended.net>
2016-02-20 22:12:06 -0600

    Update summary for new metric styles

--- precipice-reporting/src/main/java/net/uncontended/precipice/reporting/registry/Summary.java
+++ precipice-reporting/src/main/java/net/uncontended/precipice/reporting/registry/Summary.java
@@ -17,70 +17,67 @@
 package net.uncontended.precipice.reporting.registry;
 
 import net.uncontended.precipice.Failable;
+import net.uncontended.precipice.GuardRail;
+import net.uncontended.precipice.metrics.*;
 
-public class Summary<T extends Enum<T> & Failable> {
-//    private final long period;
-//    private final TimeUnit unit;
-//    private final RollingCountMetrics<T> countMetrics;
-//    private final Service service;
-//
-//    public long pendingCount = 0;
-//    public long remainingCapacity = 0;
-//
-//    public final long[] totalMetricCounts;
-//    public final long[] metricCounts;
-//
-//    public LatencySnapshot successLatency = LatencyMetrics.DEFAULT_SNAPSHOT;
-//    public LatencySnapshot errorLatency = LatencyMetrics.DEFAULT_SNAPSHOT;
-//    public LatencySnapshot timeoutLatency = LatencyMetrics.DEFAULT_SNAPSHOT;
-//
-//    public LatencySnapshot totalSuccessLatency = LatencyMetrics.DEFAULT_SNAPSHOT;
-//    public LatencySnapshot totalErrorLatency = LatencyMetrics.DEFAULT_SNAPSHOT;
-//    public LatencySnapshot totalTimeoutLatency = LatencyMetrics.DEFAULT_SNAPSHOT;
-//    private final Class<T> metricType;
-//
-//    Summary(long period, TimeUnit unit, RollingCountMetrics<T> metrics, Service service) {
-//        this.period = period;
-//        this.unit = unit;
-//        this.service = service;
-//        countMetrics = metrics;
-//        remainingCapacity = service.remainingCapacity();
-//        metricType = metrics.getMetricType();
-//        totalMetricCounts = new long[metricType.getEnumConstants().length];
-//        metricCounts = new long[metricType.getEnumConstants().length];
-//    }
-//
-//    public void refresh() {
-//        pendingCount = service.pendingCount();
-//        remainingCapacity = service.remainingCapacity();
-//
-//        MetricCounter<T> totalMetricCounter = countMetrics.totalCountMetricCounter();
-//
-//        for (T t : metricType.getEnumConstants()) {
-//            int metricIndex = t.ordinal();
-//            metricCounts[metricIndex] = 0;
-//            totalMetricCounts[metricIndex] = totalMetricCounter.getMetricCountForPeriod(t);
-//        }
-//
-//        for (MetricCounter<T> m : countMetrics.metricCounters(period, unit)) {
-//            for (T t : metricType.getEnumConstants()) {
-//                metricCounts[t.ordinal()] += m.getMetricCountForPeriod(t);
-//            }
-//        }
-//
-//        LatencyMetrics latencyMetrics = service.getLatencyMetrics();
-//
-//        if (metricType.isInstance(TimeoutableResult.class)) {
-//            if (latencyMetrics instanceof IntervalLatencyMetrics) {
-//                IntervalLatencyMetrics<TimeoutableResult> intervalMetrics = (IntervalLatencyMetrics<TimeoutableResult>) latencyMetrics;
-//                successLatency = intervalMetrics.intervalSnapshot(TimeoutableResult.SUCCESS);
-//                errorLatency = intervalMetrics.intervalSnapshot(TimeoutableResult.ERROR);
-//                timeoutLatency = intervalMetrics.intervalSnapshot(TimeoutableResult.TIMEOUT);
-//            }
-//
-//            totalSuccessLatency = latencyMetrics.latencySnapshot(TimeoutableResult.SUCCESS);
-//            totalErrorLatency = latencyMetrics.latencySnapshot(TimeoutableResult.ERROR);
-//            totalTimeoutLatency = latencyMetrics.latencySnapshot(TimeoutableResult.TIMEOUT);
-//        }
-//    }
+import java.util.concurrent.TimeUnit;
+
+public class Summary<Result extends Enum<Result> & Failable, Rejected extends Enum<Rejected>> {
+    private final long period;
+    private final TimeUnit unit;
+    private final GuardRail<Result, Rejected> guardRail;
+    private final CountMetrics<Result> resultMetrics;
+
+    public final long[] totalMetricCounts;
+    public final long[] metricCounts;
+
+    public final LatencySnapshot[] latencies;
+    public final LatencySnapshot[] totalLatencies;
+
+    Summary(long period, TimeUnit unit, GuardRail<Result, Rejected> guardRail) {
+        this.period = period;
+        this.unit = unit;
+        this.guardRail = guardRail;
+        resultMetrics = guardRail.getResultMetrics();
+        int length = resultMetrics.getMetricType().getEnumConstants().length;
+        totalMetricCounts = new long[length];
+        metricCounts = new long[length];
+        latencies = new LatencySnapshot[length];
+        totalLatencies = new LatencySnapshot[length];
+        for (int i = 0; i < length; ++i) {
+            latencies[i] = LatencyMetrics.DEFAULT_SNAPSHOT;
+            totalLatencies[i] = LatencyMetrics.DEFAULT_SNAPSHOT;
+        }
+    }
+
+    public void refresh() {
+
+        for (Result t : resultMetrics.getMetricType().getEnumConstants()) {
+            int metricIndex = t.ordinal();
+            metricCounts[metricIndex] = 0;
+            totalMetricCounts[metricIndex] = resultMetrics.getMetricCount(t);
+        }
+
+        if (resultMetrics instanceof RollingCountMetrics) {
+            RollingCountMetrics<Result> rollingMetrics = (RollingCountMetrics<Result>) resultMetrics;
+            for (CountMetrics<Result> m : rollingMetrics.metricCounters(period, unit)) {
+                for (Result t : resultMetrics.getMetricType().getEnumConstants()) {
+                    metricCounts[t.ordinal()] += m.getMetricCount(t);
+                }
+            }
+        }
+
+        LatencyMetrics<Result> latencyMetrics = guardRail.getLatencyMetrics();
+
+        if (latencyMetrics instanceof IntervalLatencyMetrics) {
+            IntervalLatencyMetrics<Result> intervalMetrics = (IntervalLatencyMetrics<Result>) latencyMetrics;
+            for (Result t : resultMetrics.getMetricType().getEnumConstants()) {
+                latencies[t.ordinal()] = intervalMetrics.intervalSnapshot(t);
+            }
+        }
+
+        for (Result t : resultMetrics.getMetricType().getEnumConstants()) {
+            totalLatencies[t.ordinal()] = latencyMetrics.latencySnapshot(t);
+        }
+    }
 }
