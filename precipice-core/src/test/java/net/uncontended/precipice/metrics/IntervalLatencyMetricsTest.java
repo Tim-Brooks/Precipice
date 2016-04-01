79e54c15e31e80cb5263cc5fb9425657c0f00407
Tim Brooks <tim@uncontended.net>
2016-03-31 20:49:06 -0500

    Fix test name

--- precipice-core/src/test/java/net/uncontended/precipice/metrics/IntervalLatencyMetricsTest.java
+++ precipice-core/src/test/java/net/uncontended/precipice/metrics/IntervalLatencyMetricsTest.java
@@ -23,7 +23,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class RecorderLatencyMetricsTest {
+public class IntervalLatencyMetricsTest {
 
     private IntervalLatencyMetrics<TestResult> metrics;
 
