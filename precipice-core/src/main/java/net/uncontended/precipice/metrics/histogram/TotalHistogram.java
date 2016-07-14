6b446baf06c52456bbe65e7158b159deb9aba631
Tim Brooks <tim@uncontended.net>
2016-07-14 17:33:36 -0500

    Add total histogram

---
+++ precipice-core/src/main/java/net/uncontended/precipice/metrics/histogram/TotalHistogram.java
@@ -0,0 +1,39 @@
+package net.uncontended.precipice.metrics.histogram;
+
+import net.uncontended.precipice.metrics.AbstractMetrics;
+import org.HdrHistogram.Histogram;
+
+public class TotalHistogram<T extends Enum<T>> extends AbstractMetrics<T> implements WritableLatency<T>, PartitionedHistogram<T> {
+
+    private final AtomicHistogram<T> histogram;
+
+    public TotalHistogram(AtomicHistogram<T> histogram) {
+        super(histogram.getMetricClazz());
+        this.histogram = histogram;
+    }
+
+    public TotalHistogram(Class<T> clazz) {
+        super(clazz);
+        histogram = new AtomicHistogram<>(clazz);
+    }
+
+    @Override
+    public void record(T result, long number, long nanoLatency) {
+        histogram.record(result, number, nanoLatency);
+    }
+
+    @Override
+    public Histogram getHistogram(T metric) {
+        return histogram.getHistogram(metric);
+    }
+
+    @Override
+    public void reset() {
+        histogram.reset();
+    }
+
+    @Override
+    public void write(T result, long number, long nanoLatency, long nanoTime) {
+        record(result, number, nanoLatency);
+    }
+}
