c1e934ad73f0b73c39179d6493412c699f2408ff
Tim Brooks <tim@uncontended.net>
2016-08-19 21:23:47 -0500

    Finish implementing metric recorder test

--- precipice-core/src/test/java/net/uncontended/precipice/metrics/tools/MetricRecorderTest.java
+++ precipice-core/src/test/java/net/uncontended/precipice/metrics/tools/MetricRecorderTest.java
@@ -17,5 +17,77 @@
 
 package net.uncontended.precipice.metrics.tools;
 
+import net.uncontended.precipice.metrics.counts.LongAdderCounter;
+import net.uncontended.precipice.rejected.Unrejectable;
+import net.uncontended.precipice.time.Clock;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
 public class MetricRecorderTest {
+
+    @Mock
+    private LongAdderCounter<Unrejectable> active;
+    @Mock
+    private LongAdderCounter<Unrejectable> inactive;
+    @Mock
+    private Clock clock;
+    @Mock
+    private FlipControl<LongAdderCounter<Unrejectable>> flipControl;
+
+    private MetricRecorder<LongAdderCounter<Unrejectable>> recorder;
+    private long startTime = 100;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        when(clock.nanoTime()).thenReturn(startTime);
+        recorder = new MetricRecorder<>(active, inactive, flipControl, clock);
+
+        verify(flipControl).flip(active);
+        assertEquals(startTime, recorder.activeIntervalStart());
+    }
+
+    @Test
+    public void activeIntervalIsFromFlipControl() {
+        when(flipControl.active()).thenReturn(active);
+        assertEquals(active, recorder.activeInterval());
+    }
+
+    @Test
+    public void captureReturnsActiveAndReusesInactive() {
+        when(flipControl.flip(inactive)).thenReturn(active);
+        LongAdderCounter<Unrejectable> counter = recorder.captureInterval(200L);
+        verify(inactive).reset();
+        assertEquals(active, counter);
+        assertEquals(200L, recorder.activeIntervalStart());
+
+        when(flipControl.flip(active)).thenReturn(inactive);
+        counter = recorder.captureInterval(300L);
+        verify(active).reset();
+        assertEquals(inactive, counter);
+        assertEquals(300L, recorder.activeIntervalStart());
+    }
+
+    @Test
+    public void captureReturnsUsesCounterIfPassed() {
+        LongAdderCounter<Unrejectable> newCounter = mock(LongAdderCounter.class);
+        recorder.captureInterval(newCounter, 200L);
+        verify(flipControl).flip(newCounter);
+    }
+
+    @Test
+    public void startAndEndDelegatesToFlipControl() {
+        when(flipControl.startRecord()).thenReturn(20L);
+        long permit = recorder.startRecord();
+        assertEquals(20L, permit);
+        verify(flipControl).startRecord();
+
+        recorder.endRecord(permit);
+        verify(flipControl).endRecord(permit);
+    }
 }
